6658b87bf41d6fbb3537a0dd71143efd64a0c295
unknown <alexanderkevindiaz05@gmail.com>
2022-10-27 14:27:34 -0600

    fix

--- app/portfolio.py
+++ app/portfolio.py
@@ -1,3 +1,4 @@
+
 from flask import(
     Blueprint, render_template
 )
@@ -7,3 +8,7 @@ bp = Blueprint('portfolio',__name__, url_prefix='/')
 @bp.route('/', methods=['GET'])
 def index():
     return render_template('portfolio/index.html')
+
+@bp.route('/mail', methods=['POST'])
+def mail():
+    return 'lala'
